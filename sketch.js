8a0ce9d78f5921482683d6f1420fd5260e8d0154
Samuel Antunes <antunessamuel0@gmail.com>
2021-04-23 14:14:04 -0300

    finished project

--- sketch.js
+++ sketch.js
@@ -1,39 +1,140 @@
+const USER = 0;
+const FOURIER = 1;
+const finishedBtn = document.getElementById('finished');
+const resetBtn = document.getElementById('reset');
+
+let y = [];
+let x = [];
+let fourierY;
+let fourierX;
 let time = 0;
-let wave = [];
+let path = [];
+let drawing = [];
+let state = USER;
+
+finishedBtn.addEventListener('click', () => {
+	state = FOURIER;
+
+	const skip = 1;
+	for(let i = 0; i < drawing.length; i+=skip){
+		x[i] = drawing[i].x;
+		y[i] = drawing[i].y;
+	}
+
+	fourierX = dft(x);
+	fourierY = dft(y);
+
+	fourierX.sort((a,b) => b.amp-a.amp);
+	fourierY.sort((a,b) => b.amp-a.amp);
+});
+
+resetBtn.addEventListener('click', () => {
+	drawing = [];
+	x = [];
+	y = [];
+	time = 0;
+	path = [];
+});
+
+function mousePressed() {
+	state = USER;
+}
+
+function mouseReleased() {
+	state = -1;
+}
+
+
+
+function epiCycles(x, y, rotation, fourier) {
+
+	
+
+	for (let i = 0; i < fourier.length; i++){
+		let prevx = x;
+		let prevy = y;
+
+		let freq = fourier[i].freq;
+		let radius = fourier[i].amp;
+		let phase = fourier[i].phase;
+
+		x += radius * cos(freq * time + phase + rotation);
+		y += radius * sin(freq * time + phase + rotation);
+	
+		stroke(255, 100);
+		noFill();
+		ellipse(prevx, prevy ,radius*2);
+		stroke(255);
+		line(prevx,prevy,x,y);
+
+	}
+
+	return createVector(x, y);
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight*0.9);
+  }
 
 function setup() {
-	createCanvas(600, 400);
+	createCanvas(windowWidth, windowHeight*0.9);
+	state = -1;
 }
+
+
 
 function draw() {
 	background(0);
-	translate(200, 200);
-	let radius = 100;
 
-	stroke(255);
-	noFill();
-	ellipse(0,0,radius*2);
+	if(state == USER) {
+		let point = createVector(mouseX - width/2, mouseY-height/2);
+		drawing.push(point);
+		stroke(255);
+		noFill();
+		beginShape();
+		for(let v of drawing){
+			vertex(v.x + width/2, v.y+height/2);
+		}
+		endShape();
+	} else if( state == FOURIER){
+		let vx = epiCycles(width/2, 100, 0, fourierX);
+		let vy = epiCycles(100, height/2, HALF_PI,fourierY);
 
-	let x = radius * cos(time);
-	let y = radius * sin(time);
-	wave.unshift(y);
-	fill(255);
-	line(0,0,x,y);
-	ellipse(x,y,8);
+		let v = createVector(vx.x, vy.y);
 
-	translate(200, 0);
-	line(x-200, y, 0,wave[0]);
-	beginShape();
-	noFill();
-	for(let i=0; i<wave.length; i++){
-		vertex(i, wave[i]);
+		line(vx.x, vx.y, v.x,v.y);
+		line(vy.x, vy.y, v.x,v.y);
+		path.unshift(v);
+
+		beginShape();
+		noFill();
+		for(let i=0; i<path.length; i++){
+			vertex(path[i].x, path[i].y);
+		}
+
+		endShape();
+
+		const dt = TWO_PI/fourierY.length
+
+		time += dt;
+
+		if( time > TWO_PI){
+			time = 0;
+			path = [];
+		}
+	} else if(state == -1){
+		stroke(255);
+		noFill();
+		beginShape();
+		for(let v of drawing){
+			vertex(v.x + width/2, v.y+height/2);
+		}
+		endShape();
 	}
 
-	endShape();
+	
 
-	time += 0.05;
-
-	if(wave.length > 500) {
+	/* if(wave.length > 500) {
 		wave.pop();
-	}
+	} */
 }
